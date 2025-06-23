3e4d31015ffd30ef2961583ecfcb7261b29d5df4
shahid <sshaikh81793@gmail.com>
2025-06-23 17:55:41 +0530

    shahid fist commit

---
+++ src/components/Footer/Footer1.jsx
@@ -0,0 +1,11 @@
+const Footer1 = () => {
+  return (
+    <footer className="footer bg-dark py-3 ">
+      <div className="container">
+        <p className="mb-0 text-light text-center">© 2025 Vivekanand College. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer1;
